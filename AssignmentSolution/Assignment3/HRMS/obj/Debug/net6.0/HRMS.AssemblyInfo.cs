bd6b8118034149b684921e8282dc218d3b2bc338
snehalmathure <snehalmathure06@gmail.com>
2025-01-08 17:57:54 +0530

    Assignment

--- AssignmentSolution/Assignment3/HRMS/obj/Debug/net6.0/HRMS.AssemblyInfo.cs
+++ AssignmentSolution/Assignment3/HRMS/obj/Debug/net6.0/HRMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c075dee3b9f1939634427c47ac67cd1c81b013e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cf23d312487884996c5c75d884bc2987d53d5c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
